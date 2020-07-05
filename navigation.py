79fa8e190e40e5b6b1bffc51342222ab1cdde49f
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-07-05 20:04:09 +0200

    pep8

--- navigation.py
+++ navigation.py
@@ -15,20 +15,20 @@ def worker(instructions, m_id, grid_size):
         m_id {int} -- id of the mower
         grid_size {tuple} -- grid (lawn) max X and max Y.
     """
-    o = GRID[m_id][2]
-    for c in instructions:
-        if c in ['R', 'L']:
-            o = orientations[(o, c)]
+    orientation = GRID[m_id][2]
+    for instruction in instructions:
+        if instruction in ['R', 'L']:
+            orientation = orientations[(orientation, instruction)]
             c_ind = GRID[m_id]
-            GRID[m_id] = (c_ind[0], c_ind[1], o)
-        elif c == 'F':
-            mov = movements[o]
+            GRID[m_id] = (c_ind[0], c_ind[1], orientation)
+        elif instruction == 'F':
+            mov = movements[orientation]
             c_ind = GRID[m_id]
             new_pos = (c_ind[0]+mov[0], c_ind[1]+mov[1])
-            if (not new_pos in GRID.values()) and (
+            if (new_pos not in GRID.values()) and (
                     0 <= new_pos[0] <= grid_size[0]) and (
                     0 <= new_pos[1] <= grid_size[1]):
-                GRID[m_id] = (c_ind[0]+mov[0], c_ind[1]+mov[1], o)
+                GRID[m_id] = (c_ind[0]+mov[0], c_ind[1]+mov[1], orientation)
         else:
             raise ValueError("instructions should be R/L/F.")
 
@@ -57,6 +57,7 @@ if __name__ == "__main__":
 
     for i in range(n):
         PROCESSES[i].join()
-
-    for elt in GRID.values():
-        print(" ".join([str(x) for x in elt]))
+    
+    # grid has a sparse representation: {'mower_id': (X, Y, O)} 
+    for mower in GRID.values():
+        print(mower[0], " ", mower[1], " ", mower[2])
